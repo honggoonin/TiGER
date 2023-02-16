73bc4b48efd801a4b4d02cc0d7f60e0fe63f729b
wjdclrhs <37328094+wjdclrhs@users.noreply.github.com>
2023-02-17 01:30:20 +0900

    Add files via upload

---
+++ Reference_Implementation/TiGER192/params.h
@@ -0,0 +1,21 @@
+#define PARAMNAME "TiGER192"
+#define LWE_N 1024		// LWE dim and LWR dim
+#define LOG_N 10
+#define LOG_Q 8
+#define Modulus_Q 256
+#define LOG_P 7
+#define LOG_K_1 6
+#define LOG_K_2 2
+#define HR 132			// Hamming weight of coefficient vector r
+#define HS 132			// Hamming weight of coefficient vector s
+#define HE 32
+#define SEED_LEN 32
+#define KK_LEN 32
+#define size_of_delta 32
+#define MESSAGE_LEN 32
+#define _8_LOG_T 7
+#define sft (sizeof(size_t) * 4 - 1)
+
+#define PUBLICKEYSIZE 32+LWE_N 
+#define SECRETKEYSIZE LWE_N+MESSAGE_LEN 
+#define CIPHERTEXTSIZE LWE_N*2 
