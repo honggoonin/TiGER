d9ed01b2432101cc5b023fc8db5f65eaa4402824
wjdclrhs <37328094+wjdclrhs@users.noreply.github.com>
2023-07-13 00:10:18 +0900

    Update TiGER_CCAKEM.c

--- TiGER128_sk_comp(230712)/TiGER_CCAKEM.c
+++ TiGER128_sk_comp(230712)/TiGER_CCAKEM.c
@@ -50,14 +50,14 @@ int KEM_Enc(unsigned char *c, unsigned char *shared_k, const unsigned char *pk){
 int KEM_dec(unsigned char *shared_k, const unsigned char *c, const unsigned char *sk, unsigned char *pk){
 	int i, res_enc, res_dec;
 	unsigned char u[MESSAGE_LEN];
-	unsigned char sk_CPA[161];
+	unsigned char sk_CPA[SECRETKEYSIZE-MESSAGE_LEN];
 	unsigned char delta_hat[size_of_delta]={0,};	
-	unsigned char c_hat[2*LWE_N];
+	unsigned char c_hat[CIPHERTEXTSIZE];
 	unsigned char coin[32];
-	unsigned char hash_t[2*LWE_N+size_of_delta];
+	unsigned char hash_t[CIPHERTEXTSIZE+size_of_delta];
 
-	memcpy(sk_CPA, sk, 161);
-	memcpy(u, sk+161, MESSAGE_LEN);
+	memcpy(sk_CPA, sk, SECRETKEYSIZE-MESSAGE_LEN);
+	memcpy(u, sk+(SECRETKEYSIZE-MESSAGE_LEN), MESSAGE_LEN);
 
 	res_dec=Decryption(delta_hat, c, sk_CPA);
 	
@@ -72,7 +72,7 @@ int KEM_dec(unsigned char *shared_k, const unsigned char *c, const unsigned char
 	unsigned char hash_c[32];
 	sha3_256(hash_c, c, CIPHERTEXTSIZE);
 
-	int8_t selector = ct_verify(c, c_hat, 2*LWE_N);
+	int8_t selector = ct_verify(c, c_hat, CIPHERTEXTSIZE);
     ct_select((unsigned char*)hash_t, (unsigned char*)delta_hat, (unsigned char*)u, size_of_delta, selector);
 
     memcpy(hash_t+size_of_delta, hash_c, 32);
@@ -80,5 +80,4 @@ int KEM_dec(unsigned char *shared_k, const unsigned char *c, const unsigned char
 
 	return 0;
 }
-
 
